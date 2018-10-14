d25e928e755e7a694cd2c0a97b9af0064add6e73
hawill10 <jsh562@nyu.edu>
2018-10-14 11:57:40 +0400

    Update mySpeller.cpp

--- mySpeller.cpp
+++ mySpeller.cpp
@@ -11,26 +11,126 @@
 
 using namespace std;
 
+class StringNode{
+    private:
+        string element;
+        StringNode* next;
+    public:
+        string getElement() const;
+        StringNode* getNext() const;
+    
+    friend class StringLinkedList;
+};
+
+class StringLinkedList{
+    private:
+        StringNode* head;
+    public:
+        StringLinkedList();
+        ~StringLinkedList();
+        bool empty() const; //checks if the list is empty
+        void removeFront(); //removes a node in the front
+        void addAfter(string word, StringNode* current); //adds a node after the current node
+        StringNode* getFront() const; //returns the head of the linkedlist
+};
+
+int keyLength =52;
+char key [52];
+
+
+int findKey(string word); //finds the index of the array of words the code should iterate through
 
 /*get dictionary name as input and use that dictionary for the program*/
 int main(int argc, char* argv[]){
     for(int i = 0; i<argc; i++){
         cout<<argv[i]<<endl;
     }
-    char* dictionaryName;
-    int maxOutput;
+    char* dictionaryName = NULL;
+    int maxOutput = -1;
     for (int i = 0; i < argc; i++){
-        if(strncmp(argv[i],"-d",2) == 0) dictionaryName = argv[i+1];
-        if(strncmp(argv[i],"-l",2) == 0) maxOutput = stoi(argv[i+1]);
+        if(strncmp(argv[i],"-d",2) == 0 && i+1 < argc) dictionaryName = argv[i+1];
+        if(strncmp(argv[i],"-l",2) == 0 && i+1 < argc) maxOutput = stoi(argv[i+1]);
     }
-    
+    if(dictionaryName==NULL || maxOutput == -1){
+        cout<<"Usage ./mySpeller -d <dictionaryfilename> -l <numberOfWordsSearch>"<<endl;
+        return -1;
+    }
     cout<<"We are using "<<dictionaryName<<endl;
     cout<<"Maximum number of output is "<<maxOutput<<endl;
     
-    /*use dictionary argv[i+1]*/
+    //fill key
+    int count = 0;
+    for (int j = 'A'; j <= 'Z'; j++){
+        key[count] = (char)j;
+        count ++;
+    }
+    for (int k = 'a'; k <= 'z'; k++){
+        key[count] = (char)k;
+        count ++;
+    }
     
-//    /*ask for input*/
-//
+//    //test if the key array is correctly filled
+//    for(int i = 0; i < 52; i++){
+//        cout<<key[i]<<endl;
+//    }
+    
+    //add each word to a array of StringLinkedList. Each linked list involves words that start with a specific letter.
+    int length = 53;
+    StringLinkedList vocab [length];
+    int index = 0;
+    StringNode* current = vocab[index].getFront();
+    string prev;
+//    int a =0;
+    ifstream fin(dictionaryName);
+    while(fin){
+        string line;
+        getline(fin, line);
+//        cout<<line<<endl;
+        if (line.substr(0,1)<"A"){
+            if (vocab[index].empty()){
+                vocab[index].addAfter(line, current);
+                current = vocab[index].getFront();
+//                a++;
+            }else{
+            vocab[index].addAfter(line, current);
+            current = current -> getNext();
+            }
+        }
+        else{
+            if(prev != line.substr(0,1)){
+                index ++;
+                current = vocab[index].getFront();
+                prev = line.substr(0,1);
+            }
+            if (vocab[index].empty()){
+                vocab[index].addAfter(line, current);
+                current = vocab[index].getFront();
+            }else{
+                vocab[index].addAfter(line, current);
+                current = current -> getNext();
+            }
+        }
+    }
+////    Test if all words are correctly stored in a linked list
+//    for(int i = 0; i<length; i++){
+//    while(!vocab[i].empty()){
+//        string s = vocab[i].getFront() -> getElement();
+//        cout<<s<<endl;
+//        vocab[i].removeFront();
+//    }
+//    cout<<endl;
+//    }
+    
+    
+    /*ask for input*/
+//    string wordInput;
+//    cout<<"Check Spelling for: ";
+//    cin>>wordInput;
+//    for (int i = 0; i < wordInput.size() ; i++){
+//        cout<<wordInput[i];
+//    }
+//    cout<<endl;
+    
 //    /*check if input is exit or not*/
 //    while(input!=Exit){
 //
@@ -49,5 +149,53 @@ int main(int argc, char* argv[]){
 //        else{
 //
 //        }
-//    }
+    
 }
+
+int findKey(string word){
+    char firstLetter = word[0];
+    for(int i = 0; i < keyLength; i++){
+        if (firstLetter == key[i]) return i+1;
+    }
+    return 0;
+}
+
+string StringNode:: getElement() const{
+    return element;
+}
+StringNode* StringNode:: getNext() const{
+    return next;
+}
+StringLinkedList::StringLinkedList(){
+    head = NULL;
+}
+StringLinkedList::~StringLinkedList(){
+    while(!empty()){
+        removeFront();
+    }
+}
+bool StringLinkedList::empty() const{
+    return head == NULL;
+}
+void StringLinkedList::removeFront(){
+    if(head!=NULL){
+        StringNode* old = head;
+        head = head -> next;
+        delete old;
+    }
+}
+void StringLinkedList::addAfter(string word, StringNode* current ){
+    StringNode* s = new StringNode;
+    s -> element = word;
+    s -> next = NULL;
+    if(current == NULL){
+        head = s;
+    }
+    else{
+        current -> next = s;
+    }
+}
+StringNode* StringLinkedList::getFront() const{
+    return head;
+}
+
